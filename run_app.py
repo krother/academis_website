3a8c879c72261f6b6b19ae563b82a36f54729c9a
Kristian Rother <kristian@spiced-academy.com>
2019-09-16 23:30:08 +0200

    create flask templates

--- run_app.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from bottle import run
-from bottle_app import application
-
-run(application, host='localhost', port=8080, reloader=True)
--- views/testimonial.tpl
+++
@@ -1,8 +0,0 @@
-<section class="paragraph-feedback">
-<h2>Feedback</h2>
-  <p>{{ !testimonial[0] }}</p>
-  <p>
-  ({{ !testimonial[1] }})
-  </p>
-</section>
-<em>
