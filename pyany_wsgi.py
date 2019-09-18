9cd7100872bba4a767753057c734847f28bfbfc4
Kristian Rother <kristian@spiced-academy.com>
2019-09-18 18:20:52 +0200

    bottle -> flask

--- pyany_wsgi.py
+++ pyany_wsgi.py
@@ -3,20 +3,13 @@
 # It works by setting the variable 'application' to a WSGI handler of some
 # description.
 #
-# The below has been auto-generated for your Bottle project
-import bottle
-import os
+# The below has been auto-generated for your Flask project
+
 import sys
 
-# add your project directory to the sys.path
-project_home = u'/home/krother/academis_bottle'
+project_home = u'/home/krother/academis'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
-# make sure the default templates directory is known to Bottle
-templates_dir = os.path.join(project_home, 'views/')
-if templates_dir not in bottle.TEMPLATE_PATH:
-    bottle.TEMPLATE_PATH.insert(0, templates_dir)
-
-# import bottle application
-from bottle_app import application
+# import flask app but need to call it "application" for WSGI to work
+from flask_app import app as application  # noqa
