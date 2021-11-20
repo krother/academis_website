b30e4843bf05fd9dedc06013ee515aa3dd2ee5aa
Kristian Rother <kristian@spiced-academy.com>
2021-11-20 21:59:26 +0100

    try to adjust WSGI

--- pyany_wsgi.py
+++ pyany_wsgi.py
@@ -12,4 +12,4 @@ if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
 # import flask app but need to call it "application" for WSGI to work
-from flask_app import app as application  # noqa
+from academis.flask_app import app as application  # noqa
