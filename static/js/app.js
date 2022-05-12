19ca1458bdbd9dc56b53cd5a22c73938e9e9b008
luise <luisebrandenburger@gmail.com>
2022-05-12 17:10:44 +0200

    add courses image change (png to gif)

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,19 @@
     const toggler = document.getElementsByClassName("toggler")[0];
     const navLinks = document.querySelectorAll(".menu-link");
     const navbar = document.querySelector("nav");
+    const coursesImg = Array.from(document.getElementsByClassName('courses-img'));
+
+    console.log(coursesImg[0].src)
+
+    coursesImg.forEach(img => {
+        img.addEventListener('mouseover', (e) => {
+            e.target.src = "../static/images/vortex.gif"
+            setTimeout(() => {
+                e.target.src = "../static/images/science.png"
+            }, 7200)
+        })
+    });
+        
 
     toggler.addEventListener("click", () => {
         navbar.classList.toggle("off")
@@ -11,8 +24,8 @@
     navLinks.forEach((link) => {
         link.addEventListener("click", (e) => {
             toggler.checked = false;
-            navbar.classList.add("off");
-        });
-    })
+            navbar.classList.add("off")
+        })
+    });
 
 })();
